29e6bce4f027c5e9c64c6e95da83d9432face9fa
Danny <saelid2517@gmail.com>
2019-11-12 20:18:32 -0800

    added strip to ipaddress, replaced default

--- version2/views.py
+++ version2/views.py
@@ -30,7 +30,7 @@ def get_ip_address(request):
     """ use requestobject to fetch client machine's IP Address """
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
+        ip = x_forwarded_for.split(',')[0].strip()
     else:
         ip = request.META.get('REMOTE_ADDR')    ### Real IP address of client Machine
     return ip 
