68859670fb31114c6e28165cc5e348365ef7f7cb
Coco Li <kel28@uw.edu>
2019-11-15 17:14:27 -0800

    updated intro and final page

--- webapp2/settings.py
+++ webapp2/settings.py
@@ -118,9 +118,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 """
 SECURE_HSTS_PRELOAD = True
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
